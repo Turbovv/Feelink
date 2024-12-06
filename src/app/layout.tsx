8deef827d7d3f0a96a5c624a30efa78d5d2c48cd
Giga Elizbarashvili <giga.elizbarashvili14@gmail.com>
2024-12-06 23:43:30 +0400

    add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { Navbar } from "./navbar";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -17,7 +18,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <TRPCReactProvider>
+          <Navbar />
+          {children}
+          </TRPCReactProvider>
       </body>
     </html>
   );
---
+++ src/app/navbar.tsx
@@ -0,0 +1,7 @@
+
+
+export function Navbar() {
+    return <div className="bg-gray-300 px-10 h-16 items-center flex">
+        <h1>Feelink</h1>
+    </div>
+}
